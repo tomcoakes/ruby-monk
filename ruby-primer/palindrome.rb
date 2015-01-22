12564c030a06c6c5758f3783a884f022225b1029
Tom Coakes <tomcoakes@gmail.com>
2015-01-22 13:36:54 +0000

    Added comment to palindrome.rb explaining better method

--- ruby-primer/palindrome.rb
+++ ruby-primer/palindrome.rb
@@ -2,3 +2,6 @@ def palindrome?(sentence)
   sentence_stripped = sentence.downcase.split.join  # Turns into a array, then back into a string
   sentence_stripped == sentence_stripped.reverse
 end
+
+# Alternatively, I should probably have used gsub rather than turning into an array and back again.
+# Would simply have needed gsub(" ", '') to replace the spaces with an empty string.
