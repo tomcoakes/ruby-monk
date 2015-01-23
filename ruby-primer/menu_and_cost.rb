baa59aa601f791d8d31e4913adb1bed1ef4326a1
Tom Coakes <tomcoakes@gmail.com>
2015-01-23 08:20:40 +0000

    Completed menu_and_cost.rb

---
+++ ruby-primer/menu_and_cost.rb
@@ -0,0 +1,13 @@
+class Restaurant
+  def initialize(menu)
+    @menu = menu
+  end
+
+  def cost(*orders)
+    orders.inject(0) do |total_cost, order| total_cost +
+    
+    order.keys.inject(0) { |cost, key| cost + (@menu[key] * order[key]) }
+      
+    end
+  end
+end
